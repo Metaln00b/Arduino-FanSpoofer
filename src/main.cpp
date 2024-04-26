81a19ee266a9cafef6facbd55f25f35a1658c5ff
Florian <Metaln00b@web.de>
2024-04-26 20:21:25 +0200

    Read fan tacho

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 
 byte getPWM(byte pin);
-byte getTacho(byte pin);
 
 const byte pwmInputPin = 3;
-const byte tachoInputPin = 9;
+const byte tachoInputPin = 2;
 
 const byte pwmOutputPin = 6;
 const byte tachoOutputPin = 5;
+
+volatile unsigned long tachoInputCounter = 0;
+volatile unsigned long lastMicrosTachoInput;
+
+void countPulses() {
+  tachoInputCounter ++;
+}
 
 void setup() {
     pinMode(pwmOutputPin, OUTPUT);
@@ -15,38 +21,37 @@ void setup() {
 
     pinMode(tachoOutputPin, OUTPUT);
     pinMode(tachoInputPin, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(tachoInputPin), countPulses, RISING);
     Serial.begin(9600);
+
+    lastMicrosTachoInput = micros();
 }
 
 void loop() {
     byte pwm = getPWM(pwmInputPin);
-    byte tacho = getTacho(tachoInputPin);
 
-    analogWrite(pwmOutputPin, pwm);
-    analogWrite(tachoOutputPin, tacho);
+    //analogWrite(pwmOutputPin, pwm);
 
-    Serial.print("Input (PWM): ");
-    Serial.print(pwm);
-    Serial.print(" Input (Tacho): ");
-    Serial.println(tacho);
+    unsigned long currentMicros = micros();
+  
+    // Calculate RPM
+    if (currentMicros - lastMicrosTachoInput >= 1000000) {
+        Serial.print("Input (PWM): ");
+        Serial.print(pwm);
+        Serial.print(" Input (Tacho): ");
+        Serial.println((tachoInputCounter / 2) * 60);
+
+        // Reset variables
+        tachoInputCounter = 0;
+        lastMicrosTachoInput = currentMicros;
+    }
 }
 
 byte getPWM(byte pin)
 {
     unsigned long highTime = pulseIn(pin, HIGH, 50000UL);  // 50 millisecond timeout
     unsigned long lowTime = pulseIn(pin, LOW, 50000UL);  // 50 millisecond timeout
-
-    // pulseIn() returns zero on timeout
-    if (highTime == 0 || lowTime == 0)
-        return digitalRead(pin) ? 255 : 0;
-
-    return (255 * highTime) / (highTime + lowTime);
-}
-
-byte getTacho(byte pin)
-{
-    unsigned long highTime = pulseIn(pin, HIGH, 5000000UL);
-    unsigned long lowTime = pulseIn(pin, LOW, 5000000UL);
 
     // pulseIn() returns zero on timeout
     if (highTime == 0 || lowTime == 0)
